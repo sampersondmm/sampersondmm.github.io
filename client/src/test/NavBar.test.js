7de32b82c62b62cad1a168bb37a660bb8d8cb497
sam <samperson539@gmail.com>
2020-06-18 20:04:45 -0400

    Added first Jest test

---
+++ client/src/test/NavBar.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import NavBar from '../components/NavBar';
+
+test('Testing navbar', () => {
+    const test = 1;
+    expect(test).toEqual(1)
+})
